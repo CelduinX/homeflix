d4c4f4559659e4ac5a1c5e57d0f1c10ca999fe3a
CelduinX <info@it-michael.net>
2025-08-01 13:57:52 +0200

    Automatische Installation

--- autoInstall.sh
+++ autoInstall.sh
@@ -14,7 +14,7 @@ install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add Docker repository
+# Add Docker repository (Ubuntu Server only!)
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
